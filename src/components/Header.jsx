8edbc0c591d9b75435863e360dd66637c1994bf6
DaLVeRS2001 <vladbiricov@mail.ru>
2020-12-29 11:22:23 +0300

    'commit1'

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<img  src="https://yt3.ggpht.com/ytc/AAUvwnjQpUZbR79nyuZkKc85qH68HwXLcdGHrFPAPTnu=s900-c-k-c0x00ffffff-no-rj" alt=""/>
+		</header>
+	)
+}
+
+
+export default Header
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<div><img src="https://cdn.wallpapersafari.com/46/46/40fySb.jpg" alt=""/></div>
+			Main content
+			<div>
+				ava + description
+			</div>
+			<div>
+				my posts
+				<div>
+					new post
+				</div>
+				<div>
+					<div>
+						post1
+					</div>
+					<div>
+						post2
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+
+export default Profile
+
+
+
+
+
+
