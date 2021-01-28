acc9cf58d4716cd6fb2967c8e372a292d40d1501
DaLVeRS2001 <vladbiricov@mail.ru>
2021-01-28 18:51:45 +0300

    compDidMountAndGetRequestWasCreated

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -2,30 +2,66 @@ import React from "react";
 import s from './User.module.scss'
 import userImage from '../../../assets/images/userImage.png'
 
-const User = (props) => {
-	const ud = props.userData
 
-	return (
-		<div className={s.user} key={ud.id}>
-			<div className={s.avaBtn}>
-				<img src={ud.photos.small != null ? ud.photos.small : userImage} alt='it wasnt loaded'/><br/>
-				<button onClick={() => {props.toggleFollow(ud.id)} }>{ud.followed ? 'Follow' : 'Unfollow'}</button>
-			</div>
 
-			<div className={s.userInfo}>
-				<div className={s.mainInfo}>
-					<div className={s.name}>{ud.name}</div>
-					<div className={s.status}>{ud.status}</div>
+class User extends React.Component {
+
+
+
+	render = () => {
+		let ud = this.props.userData
+
+		return (
+			<div className={s.user} key={ud.id}>
+
+				<div className={s.avaBtn}>
+					<img src={ud.photos.small != null ? ud.photos.small : userImage} alt='it wasnt loaded'/><br/>
+					<button onClick={() => {this.props.toggleFollow(ud.id)} }>{ud.followed ? 'Follow' : 'Unfollow'}</button>
 				</div>
-				<div className={s.location}>
-					<div className={s.country}>{'ud.location.country'},</div>
-					<div className={s.city}>{'ud.location.city'}</div>
-				</div>
-			</div>
 
-		</div>
-	)
+				<div className={s.userInfo}>
+					<div className={s.mainInfo}>
+						<div className={s.name}>{ud.name}</div>
+						<div className={s.status}>{ud.status}</div>
+					</div>
+					<div className={s.location}>
+						<div className={s.country}>{'ud.location.country'},</div>
+						<div className={s.city}>{'ud.location.city'}</div>
+					</div>
+				</div>
+
+			</div>
+		)
+	}
+
 }
+
+
+//
+// const User = (props) => {
+// 	const ud = props.userData
+//
+// 	return (
+// 		<div className={s.user} key={ud.id}>
+// 			<div className={s.avaBtn}>
+// 				<img src={ud.photos.small != null ? ud.photos.small : userImage} alt='it wasnt loaded'/><br/>
+// 				<button onClick={() => {props.toggleFollow(ud.id)} }>{ud.followed ? 'Follow' : 'Unfollow'}</button>
+// 			</div>
+//
+// 			<div className={s.userInfo}>
+// 				<div className={s.mainInfo}>
+// 					<div className={s.name}>{ud.name}</div>
+// 					<div className={s.status}>{ud.status}</div>
+// 				</div>
+// 				<div className={s.location}>
+// 					<div className={s.country}>{'ud.location.country'},</div>
+// 					<div className={s.city}>{'ud.location.city'}</div>
+// 				</div>
+// 			</div>
+//
+// 		</div>
+// 	)
+// }
 
 
 export default User
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,25 +3,50 @@ import s from './Users.module.scss'
 import User from "./User/User";
 import * as axios from "axios";
 
+class Users extends React.Component {
+	componentDidMount() {
+		axios.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(response => {
+				this.props.addUsers(response.data.items)
+			})
+	}
 
-const Users = (props) => {
-if (props.usersData.length === 0){
-	axios.get('https://social-network.samuraijs.com/api/1.0/users')
-		.then(response => {
-			props.addUsers(response.data.items)
-		})
-}
-
-
-	return (
-		<div className={s.usersBlock}>
-			Users
-			<div className={s.users}>
-				{props.usersData.map(u => <User userData={u} toggleFollow={props.toggleFollow}/>)}
+	render = () => {
+		return (
+			<div className={s.usersBlock}>
+				Users
+				<div className={s.users}>
+					{this.props.usersData.map(u => <User userData={u} toggleFollow={this.props.toggleFollow}/>)}
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
+
 }
+
+
+
+
+
+// const Users = (props) => {
+// if (props.usersData.length === 0){
+// 	axios.get('https://social-network.samuraijs.com/api/1.0/users')
+// 		.then(response => {
+// 			props.addUsers(response.data.items)
+// 		})
+// }
+//
+//
+// 	return (
+// 		<div className={s.usersBlock}>
+// 			Users
+// 			<div className={s.users}>
+// 				{props.usersData.map(u => <User userData={u} toggleFollow={props.toggleFollow}/>)}
+// 			</div>
+// 		</div>
+// 	)
+// }
+
 
 
 export default Users
