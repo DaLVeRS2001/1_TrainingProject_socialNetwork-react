de7d1d34e9c04c7a5c8a0760a29053b0b239ca53
DaLVeRS2001 <vladbiricov@mail.ru>
2021-02-09 22:20:35 +0300

    login(auth/me)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import React from 'react';
-import Header from "./components/Header/Header";
 import Sidebar from "./components/SideBar/SideBar";
 import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
   return (
     <div className="app-wrapper">
-      <Header/>
+      <HeaderContainer/>
       <Sidebar/>
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={()=> <DialogsContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import s from './Header.module.scss'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
 	return (
 		<header className={s.header}>
 			<img  src="https://yt3.ggpht.com/ytc/AAUvwnjQpUZbR79nyuZkKc85qH68HwXLcdGHrFPAPTnu=s900-c-k-c0x00ffffff-no-rj" alt=""/>
+
+			<div className={s.loginBlock}>
+				{props.isAuth
+					? props.login
+					: <NavLink to={'/login'}>Login</NavLink>
+				}
+
+			</div>
 		</header>
 	)
 }
